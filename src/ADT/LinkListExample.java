75aaba84ef65bd4315b39bc522bf6904b1d832d2
Kang <ywkang0409@gmail.com>
2024-08-16 22:38:51 +0800

    adding package

--- src/ADT/LinkListExample.java
+++ src/ADT/LinkListExample.java
@@ -3,44 +3,50 @@ import entity.Donor;
 //Remove before submit
 
 /**
- * A demo class to showcase the usage of LinkedList with Integer type.
+ * Testing purpose
  */
 public class LinkListExample {
     public static void main(String[] args) {
         // Create a new LinkedList of Integer type
-        LinkedList<Donor> integerList = new LinkedList<>();
+        LinkedList<Donor> donorList = new LinkedList<>();
+        LinkedList<Integer> integerList = new LinkedList<>();
         
         // Add elements to the list
-        integerList.add(new Donor("Name","email",01,"address")); // Adds 10 at the end
+        donorList.add(new Donor("Name1","email",01,"address")); 
+        donorList.add(new Donor("Name2","email",01,"address")); 
+        donorList.getEntry(1).setName("Name10");
+        System.out.println("Get Donor Name : " + donorList.getEntry(1).getName());
+        System.out.println("Get Donor Name : " + donorList.getEntry(2).getName());
+        
         //integerList.add("20"); // Adds 20 at the end
         
         // Print the list
-        System.out.println("List after adding 10 and 20:");
-        printList(integerList);
+        //System.out.println("List after adding 10 and 20:");
+        //printList(integerList);
         
         // Add an element at a specific position
         //integerList.add(2, 15); // Adds 15 at position 2
         
         // Print the list
-        System.out.println("List after adding 15 at position 2:");
-        printList(integerList);
+        //System.out.println("List after adding 15 at position 2:");
+        //printList(integerList);
         
         // Get an element
-        System.out.println("Element at position 2: " + integerList.getEntry(2));
+        //System.out.println("Element at position 2: " + integerList.getEntry(2));
         
         // Remove an element
-        integerList.remove(2); // Removes the element at position 2
+       // integerList.remove(2); // Removes the element at position 2
         
         // Print the list
-        System.out.println("List after removing element at position 2:");
-        printList(integerList);
+        //System.out.println("List after removing element at position 2:");
+        //printList(integerList);
         
         // Replace an element
         //integerList.replace(1, 100); // Replaces element at position 1 with 100
         
         // Print the list
-        System.out.println("List after replacing element at position 1 with 100:");
-        printList(integerList);
+        //System.out.println("List after replacing element at position 1 with 100:");
+        //printList(integerList);
         
         // Check if the list contains an element
         //System.out.println("Does the list contain 100? " + integerList.contains(100));
@@ -49,11 +55,11 @@ public class LinkListExample {
         //System.out.println("Does the list contain 10? " + integerList.contains(10));
         
         // Clear the list
-        integerList.clear();
+        //integerList.clear();
         
         // Print the list
-        System.out.println("List after clearing:");
-        printList(integerList);
+        //System.out.println("List after clearing:");
+        //printList(integerList);
     }
     
     // Helper method to print the list
---
+++ src/entity/Donee.java
@@ -0,0 +1,89 @@
+package entity;
+
+public class Donee {
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private String doneeType;  // Individual / Organization
+    private String organizationName; // Optional
+
+    public Donee(String name, String address, String phoneNumber, String email, String doneeType) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.doneeType = doneeType;
+        this.organizationName = "";
+    }
+    public Donee(String name, String address, String phoneNumber, String email, String doneeType,String organizationName) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.doneeType = doneeType;
+        this.organizationName = organizationName;
+    }
+
+    // Getters and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDoneeType() {
+        return doneeType;
+    }
+
+    public void setDoneeType(String doneeType) {
+        this.doneeType = doneeType;
+    }
+    
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    @Override
+    public String toString() {
+        return "Donee{" +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", doneeType='" + doneeType + '\'' +
+                ", Organization Name='" + organizationName + '\'' +
+                '}';
+    }
+}
